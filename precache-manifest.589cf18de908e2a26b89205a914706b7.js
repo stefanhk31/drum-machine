28d24a2ceb739c914f9e678e884ea660bd8932f7
Stefan <shodgeskluck@gmail.com>
2018-10-19 11:26:53 -0400

    Updates

---
+++ precache-manifest.589cf18de908e2a26b89205a914706b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drum-machine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "f879ea0a94edab495376",
+    "url": "/drum-machine/static/js/main.f879ea0a.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/drum-machine/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "f879ea0a94edab495376",
+    "url": "/drum-machine/static/css/main.c7c124cb.chunk.css"
+  },
+  {
+    "revision": "8b3ba4d3b18abc28e26cbf397a050bcc",
+    "url": "/drum-machine/index.html"
+  }
+];
